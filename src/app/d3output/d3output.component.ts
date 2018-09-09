5cdbd96deb889d126937b256de83ec11b954f15d
Umitosan <yeahdom@gmail.com>
2018-09-09 13:36:57 -0700

    add styles

--- src/app/d3output/d3output.component.ts
+++ src/app/d3output/d3output.component.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 import { D3Service, D3, Selection } from 'd3-ng2-service'; // <-- import the D3 Service, the type alias for the d3 variable and the Selection interface
 import { Satellite } from '../satellite.model';
 import { SatelliteService } from '../satellite.service';
-// import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-d3output',
@@ -15,10 +14,6 @@ export class D3outputComponent implements OnInit {
   private d3: D3; // <-- Define the private member which will hold the d3 reference
   satToDisplay: any;
 
-  // constructor(element: ElementRef, d3Service: D3Service,
-  //             private router: Router,
-  //             private database: AngularFireDatabase,
-  //             private satelliteService: SatelliteService) {
   constructor(element: ElementRef, d3Service: D3Service,
               private router: Router,
               private satelliteService: SatelliteService) {
@@ -28,10 +23,6 @@ export class D3outputComponent implements OnInit {
 
   ngOnInit() {
     let d3 = this.d3; // <-- for convenience use a block scope variable
-    // this.satToDisplay = this.satelliteService.getSatelliteById('0');
-    // this.satelliteService.getSatelliteById('0').subscribe(dataLastEmittedFromObserver => {
-    //   this.satToDisplay = dataLastEmittedFromObserver;
-    // })
   }
 
 }
--- src/app/satellite.service.ts
+++ src/app/satellite.service.ts
@@ -1,18 +1,16 @@
 
 import { Injectable } from '@angular/core';
 import { Satellite } from './satellite.model';
-// import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class SatelliteService {
-  // satellites: FirebaseListObservable<any[]>;
   satellites: AngularFireList<any[]>;
 
   constructor(private db: AngularFireDatabase) {
     this.satellites = db.list('satellites');
-    console.log(this.satellites);
+    // console.log(this.satellites);
   }
 
   getSatellites() {
@@ -20,30 +18,3 @@ export class SatelliteService {
   }
 
 }
-
-// old firebase
-
-
-// import { Injectable } from '@angular/core';
-// import { Satellite } from './satellite.model';
-// import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-//
-// @Injectable()
-//
-// export class SatelliteService {
-//   satellites: FirebaseListObservable<any[]>;
-//
-//   constructor(private database: AngularFireDatabase) {
-//     this.satellites = database.list('satellites');
-//   }
-//
-//   getSatellites() {
-//     return this.satellites;
-//   }
-//
-//   // getSatelliteById(someid: string){
-//   //   return this.database.object('/satellites/' + someid);
-//   // }
-//
-//
-// }
